c57424b88ff08b12984559d377005543a6b0d52e
dwaynemaster007 <thubelihledlamini88@gmail.com>
2026-09-09 23:10:05 +0200

    feat: complete week 2 learning activities

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+import { getAllOrganizations } from './src/models/organizations.js';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
 
 // Environment & Port setup
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -29,8 +31,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+  const organizations = await getAllOrganizations();
+  const title = 'Our Partner Organizations';
+
+  res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
@@ -44,8 +48,13 @@ app.get('/categories', async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
 
